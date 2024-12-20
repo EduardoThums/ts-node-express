d34078a22b159311a24dcb5cef8447d9d99e1f1d
Eduardo Cristiano Thums <eduardo.thums@cwi.com.br>
2024-12-20 15:57:08 -0300

    add config package

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express, { Express, Request, Response } from 'express';
+import config from 'config';
 import { hello } from '@services/a';
 // import dotenv from 'dotenv';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the file path and directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
+
+
+var newObject = config.util.extendDeep(config, {DB_PASSWORD: "12346"});
+
+console.log(newObject.DB_PASSWORD)
+// process.env["NODE_CONFIG_DIR"] = __dirname + "../configDir/";
+// console.log(process.env["NODE_CONFIG_DIR"])
 // dotenv.config();
 
 const app: Express = express();
@@ -9,7 +25,8 @@ const port = 3000;
 
 app.get('/ping', (req: Request, res: Response) => {
   // throw new Error("hhahaha");
-  hello()
+  console.log(config.get('Customer.credit.initialDays'))
+  // hello()
   res.send('pong');
 });
 
