edfcf61aa02645e1fb074b48c9510b38e6f0239c
Eduardo Cristiano Thums <eduardo.thums@cwi.com.br>
2024-12-23 12:03:18 -0300

    use cache middlware in specific path

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,62 @@
 import express, { Express, Request, Response } from 'express';
+// import express, { Express, Request, Response, NextFunction } from 'express';
 import config from 'config';
+// import { configure as initLog } from 'cwilog';
+import { configure } from 'cwisite';
+import { cache } from 'cwisite/cache';
 // import { hello } from '@services/a';
 // import dotenv from 'dotenv';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import { Logger } from '@aws-lambda-powertools/logger';
 
 // Get the file path and directory name
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-console.log(__dirname);
+// console.log(__dirname);
 
-const newObject = config.util.extendDeep(config, { DB_PASSWORD: '12346' });
+config.util.extendDeep(config, { DB_PASSWORD: '12346' });
 
-console.log(newObject.DB_PASSWORD);
+// console.log(newObject.DB_PASSWORD);
 // process.env["NODE_CONFIG_DIR"] = __dirname + "../configDir/";
 // console.log(process.env["NODE_CONFIG_DIR"])
 // dotenv.config();
 
 const app: Express = express();
+// app.
+
+// const globalLogger = (req: Request, res: Response, next: NextFunction) => {
+//   res.locals.logger = new Logger();
+//   next();
+// };
+
+configure(app);
+// app.use(initLog);
+
+// const logger = new Logger({ logLevel: 'INFO' });
+
 const port = 3000;
 
-app.get('/ping', (req: Request, res: Response) => {
+// function cache(ttlSeconds: number) {
+//   return function (req: Request, res: Response, next: NextFunction) {
+//     console.log('applying ttl for ' + ttlSeconds);
+//     next();
+//   };
+// }
+
+app.get('/ping', cache(100), (req: Request, res: Response) => {
   // throw new Error("hhahaha");
-  console.log(config.get('Customer.credit.initialDays'));
+  // console.log(config.get('Customer.credit.initialDays'));
+  // res.locals.logger.info('hello world from ping');
+
+  try {
+    throw new Error('vishsss');
+  } catch (error) {
+    console.log(error);
+    // res.locals.logger.error('meu primeiro erro', error as Error);
+  }
   // hello()
   res.send('pong');
 });
